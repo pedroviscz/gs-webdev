582101fdea349498ec8481670a8c6ac3a205eec4
Guilherme Godoy <guilherme0510g@gmail.com>
2025-06-06 23:03:44 -0300

    correção na indentação de alguns códigos e remoção de código comentado

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import { headerChange } from "./headerChange.js";
-import { tecnologiesDescription } from "./cards.js";
-import { slideShow } from "./slideshow.js";
-import { toggleTheme, getCurrentTheme } from "./toggleTheme.js";
-import { quiz } from "./quiz.js"
-import { mobileMenu } from "./menu.js";
+import { headerChange } from './headerChange.js';
+import { tecnologiesDescription } from './cards.js';
+import { slideShow } from './slideshow.js';
+import { toggleTheme } from './toggleTheme.js';
+import { quiz } from './quiz.js';
+import { mobileMenu } from './menu.js';
 
 mobileMenu();
 toggleTheme();
